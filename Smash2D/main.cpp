768cb7eb6e61bb622da58be433a2941ca1859120
superstevebros <srbastin@iupui.edu>
2015-04-24 23:13:29 -0400

    Initial commit

---
+++ Smash2D/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Smash2D
+//
+//  Created by Stevie Bastin on 4/24/15.
+//  Copyright (c) 2015 Super Steve Bros. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
